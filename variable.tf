7435614e865231462b535290250ba1b3d20c7186
arbazm10 <arbazmtn@gmail.com>
2024-01-06 18:56:02 +0530

    added new

--- variable.tf
+++ variable.tf
@@ -6,14 +6,4 @@ resource "azurerm_resource_group" "app11grp" {
   name     = "byterraapp-1111grp"
   location = "North Europe"
 }
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>3.8.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-}
+
