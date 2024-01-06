14e9c84907c3b0d4e0a1cbe688c629caff72fe0a
arbazm10 <arbazmtn@gmail.com>
2024-01-06 18:51:19 +0530

    both updated

--- main.tf
+++ main.tf
@@ -1,1 +1,4 @@
-
+resource "azurerm_resource_group" "app11grp" {
+  name     = "byterraapp-1134grp"
+  location = "North Europe"
+}
--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "app11grp" {
-  name     = "byterraapp-11grp"
+  name     = "byterraapp-1111grp"
   location = "North Europe"
 }
