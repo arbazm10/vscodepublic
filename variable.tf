1ed2b30b039937107354b65f0309838c1b056ff1
arbazm10 <123230684+arbazm10@users.noreply.github.com>
2024-01-06 18:52:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "app11grp" {
   name     = "byterraapp-1111grp"
   location = "North Europe"
 }
+resource "azurerm_resource_group" "app11grp" {
+  name     = "byterraapp-1111grp"
+  location = "North Europe"
+}
