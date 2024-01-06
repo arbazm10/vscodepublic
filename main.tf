8d7291d7bd400d4eb4c41306e03b085996ee7d80
arbazm10 <123230684+arbazm10@users.noreply.github.com>
2024-01-06 18:52:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "app11grp" {
   name     = "byterraapp-1134grp"
   location = "North Europe"
 }
+resource "azurerm_resource_group" "app11grp" {
+  name     = "byterraapp-1134grp"
+  location = "North Europe"
+}
+
