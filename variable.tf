0d03cec24a81a28c5f32de7c1ed56660ea10e258
arbazm10 <123230684+arbazm10@users.noreply.github.com>
2024-01-06 19:04:39 +0530

    Update variable.tf both

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "app2" {
-  name     = "byterraapp3"
+resource "azurerm_resource_group" "app6" {
+  name     = "byterraapp6"
   location = "North Europe"
 }
-resource "azurerm_resource_group" "app3" {
-  name     = "byterraapp3"
+resource "azurerm_resource_group" "app7" {
+  name     = "byterraapp7"
   location = "North Europe"
 }
 
